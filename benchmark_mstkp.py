f28edf2ee0721887a3a916f91d0ddf62d4855117
somayehshamsi <somayeh.shamsi@monash.edu>
2025-12-05 11:00:33 +1100

    changebenchmark

--- benchmark_mstkp.py
+++ benchmark_mstkp.py
@@ -393,83 +393,8 @@ def main():
 
     # Define configurations with varied cover_cuts and inherit_lambda (kept as you provided)
     configs = [
-        # {
-        #     "branching_rule": "most_fractional",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
-        # ,
-        # {
-        #     "branching_rule": "reliability",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
-        #         ,
-
-        # {
-        #     "branching_rule": "hybrid_strong_fractional",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
-
-        # ,
-        # {
-        #     "branching_rule": "random_mst",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
-        # , 
-        # {
-        #     "branching_rule": "random_fractional",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
-        # ,
-        # {
-        #     "branching_rule": "sb_fractional",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
-        # ,
-
-        # {
-        #     "branching_rule": "strong_branching",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
-        # ,
-        
-
         {
-            "branching_rule": "random_all",
+            "branching_rule": "most_fractional",
             "use_bisection": False,
             "use_2opt": False,
             "use_shooting": False,
@@ -479,7 +404,7 @@ def main():
         }
         ,
         {
-            "branching_rule": "strong_branching_all",
+            "branching_rule": "reliability",
             "use_bisection": False,
             "use_2opt": False,
             "use_shooting": False,
@@ -487,6 +412,81 @@ def main():
             "inherit-step-size": False,
             "inherit_lambda": True
         }
+                ,
+
+        {
+            "branching_rule": "hybrid_strong_fractional",
+            "use_bisection": False,
+            "use_2opt": False,
+            "use_shooting": False,
+            "cover_cuts": True,
+            "inherit-step-size": False,
+            "inherit_lambda": True
+        }
+
+        ,
+        {
+            "branching_rule": "random_mst",
+            "use_bisection": False,
+            "use_2opt": False,
+            "use_shooting": False,
+            "cover_cuts": True,
+            "inherit-step-size": False,
+            "inherit_lambda": True
+        }
+        , 
+        {
+            "branching_rule": "random_fractional",
+            "use_bisection": False,
+            "use_2opt": False,
+            "use_shooting": False,
+            "cover_cuts": True,
+            "inherit-step-size": False,
+            "inherit_lambda": True
+        }
+        ,
+        {
+            "branching_rule": "sb_fractional",
+            "use_bisection": False,
+            "use_2opt": False,
+            "use_shooting": False,
+            "cover_cuts": True,
+            "inherit-step-size": False,
+            "inherit_lambda": True
+        }
+        ,
+
+        {
+            "branching_rule": "strong_branching",
+            "use_bisection": False,
+            "use_2opt": False,
+            "use_shooting": False,
+            "cover_cuts": True,
+            "inherit-step-size": False,
+            "inherit_lambda": True
+        }
+        # ,
+        
+
+        # {
+        #     "branching_rule": "random_all",
+        #     "use_bisection": False,
+        #     "use_2opt": False,
+        #     "use_shooting": False,
+        #     "cover_cuts": True,
+        #     "inherit-step-size": False,
+        #     "inherit_lambda": True
+        # }
+        # ,
+        # {
+        #     "branching_rule": "strong_branching_all",
+        #     "use_bisection": False,
+        #     "use_2opt": False,
+        #     "use_shooting": False,
+        #     "cover_cuts": True,
+        #     "inherit-step-size": False,
+        #     "inherit_lambda": True
+        # }
     ]
 
     results = []
