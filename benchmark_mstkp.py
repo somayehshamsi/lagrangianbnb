cc6518be2dc87890f2fdf1a4b40e261f2a4d0065
somayehshamsi <somayeh.shamsi@monash.edu>
2026-06-04 13:30:30 +1000

    changebench

--- benchmark_mstkp.py
+++ benchmark_mstkp.py
@@ -482,16 +482,16 @@ def main():
             "inherit_lambda": True
         }
 
-        # ,
-        # {
-        #     "branching_rule": "random_mst",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
+        ,
+        {
+            "branching_rule": "random_mst",
+            "use_bisection": False,
+            "use_2opt": False,
+            "use_shooting": False,
+            "cover_cuts": True,
+            "inherit-step-size": False,
+            "inherit_lambda": True
+        }
         , 
         {
             "branching_rule": "random_fractional",
@@ -512,18 +512,18 @@ def main():
             "inherit-step-size": False,
             "inherit_lambda": True
         }
-        # ,
+        ,
 
-        # {
-        #     "branching_rule": "strong_branching",
-        #     "use_bisection": False,
-        #     "use_2opt": False,
-        #     "use_shooting": False,
-        #     "cover_cuts": True,
-        #     "inherit-step-size": False,
-        #     "inherit_lambda": True
-        # }
-        # ,
+        {
+            "branching_rule": "strong_branching",
+            "use_bisection": False,
+            "use_2opt": False,
+            "use_shooting": False,
+            "cover_cuts": True,
+            "inherit-step-size": False,
+            "inherit_lambda": True
+        }
+        ,
         #  {
         #     "branching_rule": "random_mst",
         #     "use_bisection": False,
